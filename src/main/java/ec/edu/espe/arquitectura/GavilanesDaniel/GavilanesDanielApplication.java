d2e5f7f8608770ac1ce1b871804cb286cf52cf91
Daniel Alonso Gavilanes Falconi <dagavilanes2@espe.edu.ec>
2023-06-14 13:53:48 -0500

    3 update

---
+++ src/main/java/ec/edu/espe/arquitectura/GavilanesDaniel/GavilanesDanielApplication.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.GavilanesDaniel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GavilanesDanielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GavilanesDanielApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/edu/espe/arquitectura/GavilanesDaniel/GavilanesDanielApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.edu.espe.arquitectura.GavilanesDaniel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GavilanesDanielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
